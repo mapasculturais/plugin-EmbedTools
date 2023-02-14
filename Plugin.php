dfa0cb25bc44580e908e8a088dd58793566e3cdc
erleibiazzio <erleibiazzio@gmail.com>
2023-02-14 16:54:09 -0300

    Faz com que ao executar o plugin, caso encontre na URL o ide do controller, execta as visões no BaseV1 Ref.: #5

--- Plugin.php
+++ Plugin.php
@@ -8,8 +8,19 @@ class Plugin extends \MapasCulturais\Plugin
 {
     function __construct($config = [])
     {
+        /** @var  App $app */
+        $app = App::i();
+
         $config += [];
         parent::__construct($config);
+
+        $app->hook("app.init:before", function () {
+            if (strpos($_SERVER['REQUEST_URI'], '/embedtools') === 0) {
+                $theme_instance = new \MapasCulturais\Themes\BaseV1\Theme($this->config['themes.assetManager']);
+                $theme_instance->path = $this->view->path;
+                $this->view = $theme_instance;
+            }
+        });
     }
 
     public function _init()
