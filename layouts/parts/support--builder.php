cfc4e38304b86a854047bd5e247e35db3dded338
erleibiazzio <erleibiazzio@gmail.com>
2023-02-16 14:38:38 -0300

    Remove part que carrega a listagem de inscrições Ref.: #7

--- layouts/parts/support--builder.php
+++ layouts/parts/support--builder.php
@@ -1,4 +1,3 @@
 <article ng-controller="SupportForm">
         <?php $this->part('support/opportunity-support-settings', ['entity' => $entity]);?>
-        <?php $this->part('support/opportunity-support', ['entity' => $entity]);?>
     </article>
