77f90a1cd84db1d13d8ea48ed0ae4c290d27cd27
LimaSystem <wolegario49@gmail.com>
2023-02-24 12:38:53 -0500

    Refatora Endpoint comforme orientação na issue Ref.:#11

--- Controller.php
+++ Controller.php
@@ -11,62 +11,35 @@ class Controller extends \MapasCulturais\Controllers\Opportunity
     {
         $this->entityClassName = Opportunity::class;
 
-        $this->requireAuthentication();
-
         $this->layout = 'embedtools-opportunity';
     }
 
     public function GET_formbuilder()
     {
-        $app = App::i();
-
-        if ($entity = $this->requestedEntity) {
-            $app->pass();
-        }
-
-        $entity->checkPermission('@control');
+        $entity = $this->getEntityAndCheckPermission('@control');
 
         $this->render("form-builder", ['entity' => $entity]);
     }
 
     public function GET_registrationmanager()
     {
-        $app = App::i();
-
-        if (!$entity = $this->requestedEntity) {
-            $app->pass();
-        }
-
-        $entity->checkPermission('@control');
+        $entity = $this->getEntityAndCheckPermission('@control');
 
         $this->render("registration-manager", ['entity' => $entity]);
     }
 
     public function GET_supportbuilder()
     {
-
-        $app = App::i();
-
-        if (!$entity = $this->requestedEntity) {
-            $app->pass();
-        }
-
-        $entity->checkPermission('@control');
+        $entity = $this->getEntityAndCheckPermission('@control');
 
         $this->render("support-builder", ['entity' => $entity]);
     }
 
     public function GET_registrationform()
     {
-        $app = App::i();
-
         $this->entityClassName = "MapasCulturais\\Entities\\Registration";
 
-        if ($entity = $this->requestedEntity) {
-            $app->pass();
-        }
-
-        $entity->checkPermission('modify');
+        $entity = $this->getEntityAndCheckPermission('@control');
 
         $this->layout = "embedtools-registration";
         $this->render("registration-form", ['entity' => $entity]);
@@ -74,14 +47,21 @@ class Controller extends \MapasCulturais\Controllers\Opportunity
 
     public function GET_affirmativepoliciesmanager()
     {
-        $app = App::i();
-
-        if ($entity = $this->requestedEntity) {
-            $app->pass();
-        }
-
-        $entity->checkPermission('@control');
+        $entity = $this->getEntityAndCheckPermission('@control');
 
         $this->render("affirmative-policies-manager", ['entity' => $entity]);
+    }
+
+    function getEntityAndCheckPermission($permission) 
+    {
+        $app = App::i();
+
+        $this->requireAuthentication();
+        if (!$entity = $this->requestedEntity) {
+            $app->pass();
+        }
+        
+        $entity->checkPermission($permission);
+        return $entity;
     }
 }
